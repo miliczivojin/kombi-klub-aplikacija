617e11c9055bc37e9e58a016279f3561477a7d4f
zivoj <milic.zivojin.1475@gmail.com>
2026-03-22 19:33:39 +0100

    promena naziva const variable

--- src/components/Karta.jsx
+++ src/components/Karta.jsx
@@ -1,11 +1,11 @@
-const mapa =
+const MAPA =
   "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1205.524371221578!2d21.88503197884562!3d43.31598328226148!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4755b0ad36a40001%3A0xb5886b4a61694b5d!2sKombi%20Klub!5e0!3m2!1ssr!2srs!4v1768361875115!5m2!1ssr!2srs";
 
 function Karta() {
   return (
     <div className="h-100 md:h-120 rounded-none md:rounded-xl overflow-hidden shadow-[0_20px_60px_rgba(0,0,0,0.5)] bg-white">
       <iframe
-        src={mapa}
+        src={MAPA}
         allowFullScreen
         className="w-full h-full"
         referrerPolicy="no-referrer-when-downgrade"
