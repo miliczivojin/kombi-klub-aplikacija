403ef58ed1adf03ac1ed0ed8b2db21c1de99d37a
zivoj <milic.zivojin.1475@gmail.com>
2026-03-25 21:39:38 +0100

    Ispravka KlasaMeni

--- src/layouts/Header/KlasikMeni.jsx
+++ src/layouts/Header/KlasikMeni.jsx
@@ -1,29 +1,45 @@
 import { NavLink } from "react-router-dom";
 
+const klasaMeniStavka = "meni-stavka";
+const klasaMeniLink = "meni-link";
+const klasaMeniAktivan = "meni-aktivan";
+
 function KlasikMeni() {
   return (
     <nav className="hidden md:block">
-      <ul className='flex items-center justify-between'>
-        <li className='meni-stavka'>
-          <NavLink to="/" className={({ isActive }) => `meni-link ${isActive ? "meni-aktivan" : ""}`}>
+      <ul className="flex items-center justify-between">
+        <li className={klasaMeniStavka}>
+          <NavLink
+            to="/"
+            className={({ isActive }) => `${klasaMeniLink} ${isActive ? klasaMeniAktivan : ""}`}
+          >
             Početna
           </NavLink>
         </li>
 
-        <li className='meni-stavka'>
-          <NavLink to="/pronadji-delove" className={({ isActive }) => `meni-link ${isActive ? "meni-aktivan" : ""}`}>
+        <li className={klasaMeniStavka}>
+          <NavLink
+            to="/pronadji-delove"
+            className={({ isActive }) => `${klasaMeniLink} ${isActive ? klasaMeniAktivan : ""}`}
+          >
             Pronađi Delove
           </NavLink>
         </li>
 
-        <li className='meni-stavka'>
-          <NavLink to="/o-nama" className={({ isActive }) => `meni-link ${isActive ? "meni-aktivan" : ""}`}>
+        <li className={klasaMeniStavka}>
+          <NavLink
+            to="/o-nama"
+            className={({ isActive }) => `${klasaMeniLink} ${isActive ? klasaMeniAktivan : ""}`}
+          >
             O nama
           </NavLink>
         </li>
 
-        <li className='meni-stavka'>
-          <NavLink to="/kontakt" className={({ isActive }) => `meni-link ${isActive ? "meni-aktivan" : ""}`}>
+        <li className={klasaMeniStavka}>
+          <NavLink
+            to="/kontakt"
+            className={({ isActive }) => `${klasaMeniLink} ${isActive ? klasaMeniAktivan : ""}`}
+          >
             Kontakt
           </NavLink>
         </li>
