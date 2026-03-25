5d865048dc45f784e1205a5064a652b29e173658
zivoj <milic.zivojin.1475@gmail.com>
2026-03-25 21:11:57 +0100

    Ispravka ScrollToTop

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -2,11 +2,12 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 function ScrollToTop() {
-    const { pathname } = useLocation();
+    const { pathname: putanja } = useLocation();
 
     useEffect(() => {
+        // Resetovanje pozicije na vrh stranice pri promeni rute.
         window.scrollTo(0, 0);
-    }, [pathname]);
+    }, [putanja]);
 
     return null;
 }
