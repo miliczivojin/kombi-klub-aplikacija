619d1bb434d490a63ecb6700f7c00c387d3f3f91
zivoj <milic.zivojin.1475@gmail.com>
2026-03-25 21:41:35 +0100

    Ispravka Logo

--- src/layouts/Header/Logo.jsx
+++ src/layouts/Header/Logo.jsx
@@ -1,10 +1,13 @@
 import slika from "@/assets/photos/logo_beli.png";
 
+const klasaLogo =
+  "w-14 h-14 sm:w-16 sm:h-16 md:w-20 md:h-20 object-contain";
+
 function Logo() {
   return (
     <img
       loading="lazy"
-      className="w-14 h-14 sm:w-16 sm:h-16 md:w-20 md:h-20 object-contain"
+      className={klasaLogo}
       src={slika}
       alt="Logo Kombi Klub"
       width={100}
