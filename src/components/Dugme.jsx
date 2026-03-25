acc1a299cf96e2eb839eecca3572f5fdb7c0cfda
zivoj <milic.zivojin.1475@gmail.com>
2026-03-25 21:01:02 +0100

    Ispravka Dugme

--- src/components/Dugme.jsx
+++ src/components/Dugme.jsx
@@ -1,6 +1,12 @@
+const osnovnaKlasa =
+    "px-4 py-2 border-2 sm:text-lg lg:text-xl border-black rounded-4xl shadow-[2px_2px_0_0_black] text-white font-semibold select-none cursor-pointer transition";
+
 function Dugme({ children, className = "", onClick }) {
     return (
-        <button className={`px-4 py-2 border-2 sm:text-lg lg:text-xl border-black rounded-4xl shadow-[2px_2px_0_0_black] text-white font-semibold select-none cursor-pointer transition ${className}`} onClick={onClick}>
+        <button
+            className={`${osnovnaKlasa} ${className}`.trim()}
+            onClick={onClick}
+        >
             {children}
         </button>
     );
